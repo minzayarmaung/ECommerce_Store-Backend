2c8748d8972de1965f6e8af88620fc1d71a91de6
Min Zayar Maung <minzayarmaung2002@gmail.com>
2024-10-27 20:39:23 +0630

    response

--- src/main/java/com/project/storeSystem/Controller/LoginForm.java
+++ src/main/java/com/project/storeSystem/Controller/LoginForm.java
@@ -9,16 +9,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class LoginForm {
 
     @Autowired
     private ServiceLoginForm serviceLoginForm;
 
     @PostMapping("/signin")
-    public ResponseEntity<String> signin (@RequestBody User user ,
+    public ResponseEntity<Map<String , String>> signin (@RequestBody User user ,
                                           @RequestHeader(value = "Authorization" , required = false) String token) {
+
+        Map<String , String> response = new HashMap<>();
+
         System.out.println(user);
         boolean isValidLicense = false;
         boolean isValidUser = false;
@@ -26,16 +33,20 @@ public class LoginForm {
         isValidUser = serviceLoginForm.checkValidUser(user);
 
         if (token != null && TokenUtil.validToken(token, user.getUsername())) {
-            return ResponseEntity.status(HttpStatus.OK).body("Already signed in with a valid token.");
+            return ResponseEntity.ok(response);
         }
 
         if (isValidLicense && isValidUser) {
             String jwtToken = TokenUtil.generateToken(user.getUsername());
-            return ResponseEntity.status(HttpStatus.OK).body("Login Successfully.");
+            response.put("message", "Login Successfully.");
+            response.put("token", jwtToken);
+            return ResponseEntity.ok(response);
         } else if (isValidLicense && !isValidUser) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("User Not Found");
+            response.put("message", "User Not Found");
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
         } else {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid License");
+            response.put("message", "Invalid License");
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
         }
     }
 }
