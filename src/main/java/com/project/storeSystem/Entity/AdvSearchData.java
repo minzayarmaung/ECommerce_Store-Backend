ca7c81b9173749f1523b5bec36a159bdc5610272
Min Zayar Maung <minzayarmaung2002@gmail.com>
2024-11-18 22:54:36 +0630

    add

---
+++ src/main/java/com/project/storeSystem/Entity/AdvSearchData.java
@@ -0,0 +1,25 @@
+package com.project.storeSystem.Entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdvSearchData {
+
+    private String id;
+    private String name;
+    private String fromDate;
+    private String toDate;
+    private String fromTime;
+    private String toTime;
+    private String status;
+    private String branch;
+    private String city;
+    private String township;
+}
