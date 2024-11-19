3b4a4215b9157f2f1522bde6da0e3c989221c967
Min Zayar Maung <minzayarmaung2002@gmail.com>
2024-11-19 22:34:35 +0630

    add

--- src/main/java/com/project/storeSystem/Repository/InvoiceRepository.java
+++ src/main/java/com/project/storeSystem/Repository/InvoiceRepository.java
@@ -10,6 +10,12 @@ import java.util.List;
 @Repository
 public interface InvoiceRepository extends JpaRepository<Invoice , Long> {
 
-//    @Query("SELECT * FROM INV001 WHERE status <> 4")
-//    List<Invoice> getAllInvoiceItems();
+    @Query(value = "SELECT * FROM INV001 WHERE STATUS <> ?" , nativeQuery = true)
+    List<Invoice> getDatabyStatus(String status);
+
+    @Query(value = "SELECT * FROM INV001 WHERE STATUS <> ? AND ID = ? AND " , nativeQuery = true)
+    List<Invoice> getAdvSearchData(String status , String id , String name , String branch ,
+                                   String city , String township , String fromDate ,
+                                   String toDate , String fromTime, String toTime);
+
 }
