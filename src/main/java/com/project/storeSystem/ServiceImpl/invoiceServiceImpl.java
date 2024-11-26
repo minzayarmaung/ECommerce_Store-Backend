c58d4cd435a35e081f06eacd3d45a90642c0278f
Min Zayar Maung <minzayarmaung2002@gmail.com>
2024-11-26 21:56:02 +0630

    update

--- src/main/java/com/project/storeSystem/ServiceImpl/invoiceServiceImpl.java
+++ src/main/java/com/project/storeSystem/ServiceImpl/invoiceServiceImpl.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.project.storeSystem.Util.EncryptionUtil.encryptedInvoiceItems;
+
 @Service
 public class invoiceServiceImpl implements invoiceService {
 
@@ -30,47 +32,5 @@ public class invoiceServiceImpl implements invoiceService {
         return encryptedItems;
     }
 
-    private EncryptedInvoice encryptedInvoiceItems(Invoice invoice) {
-        EncryptedInvoice encryptedInvoice = new EncryptedInvoice();
-        try{
-            encryptedInvoice.setEncryptedSyskey((AESAlgorithm.encryptString(String.valueOf(invoice.getSyskey()))));
-            encryptedInvoice.setEncryptedAutokey((AESAlgorithm.encryptString(String.valueOf(invoice.getAutokey()))));
 
-            // String
-            encryptedInvoice.setCreatedDate(AESAlgorithm.encryptString(invoice.getCreatedDate()));
-            encryptedInvoice.setModifiedDate(AESAlgorithm.encryptString(invoice.getModifiedDate()));
-            encryptedInvoice.setT1(AESAlgorithm.encryptString(invoice.getT1()));
-            encryptedInvoice.setT2(AESAlgorithm.encryptString(invoice.getT2()));
-            encryptedInvoice.setT3(AESAlgorithm.encryptString(invoice.getT3()));
-            encryptedInvoice.setT4(AESAlgorithm.encryptString(invoice.getT4()));
-            encryptedInvoice.setT5(AESAlgorithm.encryptString(invoice.getT5()));
-            encryptedInvoice.setT6(AESAlgorithm.encryptString(invoice.getT6()));
-            encryptedInvoice.setT7(AESAlgorithm.encryptString(invoice.getT7()));
-            encryptedInvoice.setT8(AESAlgorithm.encryptString(invoice.getT8()));
-            encryptedInvoice.setT9(AESAlgorithm.encryptString(invoice.getT9()));
-            encryptedInvoice.setT10(AESAlgorithm.encryptString(invoice.getT10()));
-            encryptedInvoice.setT11(AESAlgorithm.encryptString(invoice.getT11()));
-            encryptedInvoice.setT12(AESAlgorithm.encryptString(invoice.getT12()));
-            encryptedInvoice.setT13(AESAlgorithm.encryptString(invoice.getT13()));
-            encryptedInvoice.setT14(AESAlgorithm.encryptString(invoice.getT14()));
-            encryptedInvoice.setT15(AESAlgorithm.encryptString(invoice.getT15()));
-
-            // Long
-            encryptedInvoice.setN1((AESAlgorithm.encryptString(String.valueOf(invoice.getN1()))));
-            encryptedInvoice.setN2((AESAlgorithm.encryptString(String.valueOf(invoice.getN2()))));
-            encryptedInvoice.setN3((AESAlgorithm.encryptString(String.valueOf(invoice.getN3()))));
-            encryptedInvoice.setN4((AESAlgorithm.encryptString(String.valueOf(invoice.getN4()))));
-            encryptedInvoice.setN5((AESAlgorithm.encryptString(String.valueOf(invoice.getN5()))));
-            encryptedInvoice.setN6((AESAlgorithm.encryptString(String.valueOf(invoice.getN6()))));
-            encryptedInvoice.setN7((AESAlgorithm.encryptString(String.valueOf(invoice.getN7()))));
-            encryptedInvoice.setN8((AESAlgorithm.encryptString(String.valueOf(invoice.getN8()))));
-            encryptedInvoice.setN9((AESAlgorithm.encryptString(String.valueOf(invoice.getN9()))));
-            encryptedInvoice.setN10((AESAlgorithm.encryptString(String.valueOf(invoice.getN10()))));
-
-        } catch(Exception e){
-            e.printStackTrace();
-            throw new RuntimeException("Error Encrypting Invoice Items");
-        }
-        return encryptedInvoice;
-    }
 }
