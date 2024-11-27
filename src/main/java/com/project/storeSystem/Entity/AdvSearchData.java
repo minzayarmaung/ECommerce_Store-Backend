a711b7977079fe48d2ce12f82813dfdcb296a382
Min Zayar Maung <minzayarmaung2002@gmail.com>
2024-11-27 23:37:36 +0630

    update

--- src/main/java/com/project/storeSystem/Entity/AdvSearchData.java
+++ src/main/java/com/project/storeSystem/Entity/AdvSearchData.java
@@ -22,4 +22,10 @@ public class AdvSearchData {
     private String branch;
     private String city;
     private String township;
+    private String gender;
+    private String maritalStatus;
+    private String religion;
+    private String phoneNumber;
+    private String email;
+
 }
